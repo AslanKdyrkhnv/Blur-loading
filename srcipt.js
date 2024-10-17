894a7a5ba652bc0ebb37d8f543480bbac21ae1ff
BigSurAI <kadyrhanov98@bk.ru>
2024-10-17 22:42:46 +0500

    v2

--- srcipt.js
+++ srcipt.js
@@ -1,15 +1,19 @@
 const bg = document.querySelector('.bg'); 
 const loadText = document.querySelector('.loading-text');
 
-let load = 100; 
+let load = 0; 
 let init = setInterval(blurring, 10)
 
 function blurring() {
-    load--;
-    if(load===0) {
+    load++;
+    if(load>99) {
         clearInterval(init);
     }
     loadText.innerHTML = `${load}%`;
-    bg.style.filter = `blur(${load}px)`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 100, 0)}px)`;
 }
 
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
